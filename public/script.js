dee6fd83642ad504221001504ac9a4dd3f607926
Dalton McMullen <thecodingegg@gmail.com>
2025-10-25 22:20:42 -0700

    Reverted back to general structure
    Don't want to make it to complicated at the beginning. Will iterate
    overtime.

---
+++ public/script.js
@@ -0,0 +1,12 @@
+console.log("Log your flight!");
+
+let flightList = [];
+
+//add a flight to the array
+function addFlight() {}
+
+// display all flights in a table format
+function displayFlights() {}
+
+// get weather data for takeoff and landing
+function getWeatherData() {}
